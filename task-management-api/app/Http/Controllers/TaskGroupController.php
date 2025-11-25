95b8586e300feedf70b0e62eb0317c223d1769e1
JM Mac <johnmarkmancol@gmail.com>
2025-11-25 10:53:09 +0800

    chore: date groups api

---
+++ task-management-api/app/Http/Controllers/TaskGroupController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TaskGroupController extends Controller
+{
+    /**
+     * Display a listing of the dates from tasks.
+     */
+    public function index()
+    {
+        // Get authenticated user's ID
+        $userId = Auth::id();
+
+        // Get distinct dates of the authenticated user's task
+        $dates = Task::selectRaw('DATE(created_at) as only_date')
+            ->where('user_id', $userId)
+            ->distinct()
+            ->orderByDesc('only_date')
+            ->pluck('only_date');
+
+        
+        return response()->json($dates);
+    }
+}
--- task-management-api/routes/api.php
+++ task-management-api/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskGroupController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/users', [UserController::class, 'index']);
 Route::apiResource('/tasks', TaskController::class)->middleware('auth:sanctum');
+Route::get('/task-groups', [TaskGroupController::class, 'index'])->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
